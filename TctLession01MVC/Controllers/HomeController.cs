e3d6f6b3d8e0cc23c0755a4298bfd280b9bac0e7
Truong Cong Tien <tiencong705@gmail.com>
2026-08-22 10:41:11 +0700

    TvcLession01

---
+++ TctLession01MVC/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TctLession01MVC.Models;
+
+namespace TctLession01MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.name = "Truong Cong Tien";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
