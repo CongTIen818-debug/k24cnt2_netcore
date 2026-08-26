4199dd1b9c097c917fa53d0a2167d4ce537d64d0
Truong Cong Tien <tiencong705@gmail.com>
2026-08-26 14:37:38 +0700

    TctLesson02

---
+++ TctLesson02/Controllers/TctProductController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TctLesson02.Models;
+
+namespace TctLesson02.Controllers
+{
+    public class TctProductController : Controller
+    {
+        public IActionResult TctIndex()
+        {
+            ViewBag.name = "Truong Cong Tien";
+            ViewData["productVD"] = "Laptop Lenovo Legion";
+            TempData["Uni"] = "Truong Dai hoc Nguyen Trai - NTU";
+
+            return View();
+        }
+
+        public IActionResult GetProduct()
+        {
+            TctProduct tctProduct = new TctProduct()
+            {
+                ProductID = "2410900074",
+                ProductName = "Truong Cong Tien",
+                YearRelease=2003,
+                Price = 1000
+            };
+            ViewBag.product = tctProduct;
+            ViewData["product"] = tctProduct;
+
+            return View("product");
+        }
+    }
+}
